4092879a407f64c562a42d823a4d441e8ee23f84
Ingu Lee <mangogoon@gmail.com>
2022-05-16 14:18:39 +0900

    ## TODO APP 개발 - styled-component 적용

---
+++ 01-starting-project/src/App.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+
+import CourseGoalList from './components/CourseGoals/CourseGoalList/CourseGoalList';
+import CourseInput from './components/CourseGoals/CourseInput/CourseInput';
+import './App.css';
+
+const DUMMY_DATA = [
+  { text: 'React 강의 듣기!', id: 'g1' },
+  { text: '운동하기!!', id: 'g2' },
+  { text: '모던자바스크립트 Deep Dive 공부하기!', id: 'g3' },
+]
+
+
+const App = () => {
+  const [courseGoals, setCourseGoals] = useState(DUMMY_DATA);
+
+  const addGoalHandler = enteredText => {
+    setCourseGoals(prevGoals => {
+      const updatedGoals = [...prevGoals];
+      updatedGoals.unshift({ text: enteredText, id: Math.random().toString() });
+      return updatedGoals;
+    });
+  };
+
+  const deleteItemHandler = goalId => {
+    setCourseGoals(prevGoals => {
+      const updatedGoals = prevGoals.filter(goal => goal.id !== goalId);
+      return updatedGoals;
+    });
+  };
+
+  let content = (
+    <p style={{ textAlign: 'center' }}>할 일을 모두 달성했습니다 :)<br/>할 일을 추가하시겠습니까?</p>
+  );
+
+  if (courseGoals.length > 0) {
+    content = (
+      <CourseGoalList items={courseGoals} onDeleteItem={deleteItemHandler} />
+    );
+  }
+
+  return (
+    <div>
+      <section id="goal-form">
+        <CourseInput onAddGoal={addGoalHandler} />
+      </section>
+      <section id="goals">
+        {content}
+        {/* {courseGoals.length > 0 && (
+          <CourseGoalList
+            items={courseGoals}
+            onDeleteItem={deleteItemHandler}
+          />
+        ) // <p style={{ textAlign: 'center' }}>No goals found. Maybe add one?</p>
+        } */}
+      </section>
+    </div>
+  );
+};
+
+export default App;
